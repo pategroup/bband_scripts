58267fc3d5cd65b0cf58c48d4d3d573a3856c31d
Nathan Seifert <nas3xf@virginia.edu>
2013-08-21 10:38:34 -0400

    pushing new progress bar framework

--- autofit/linux_x86-64/progress/progressbar.py
+++ autofit/linux_x86-64/progress/progressbar.py
@@ -1,21 +1,34 @@
 import time
 import sys
-import curses 
 
-def progress():
-	curses.initscr()
-	win = curses.newwin(3,32,14,10)
-	win.border(0)
-	win.addstr(1,1,"Progress ")
+t1 = time.time()
+t2 = time.time()
+veloc = 0
+count = 0
 
-	pos = 10
-	for i in range(15):
-		win.addstr(1,pos,".")
-		win.refresh()
-		time.sleep(0.1)
-		pos += 1
-	win.addstr(1,26,"Done!")
-	win.refresh()
-	time.sleep(0.5)
+# This works nicely!!!
+def update_progress(progress):
+	global count
+	if count == 0:
+		global t1
+		t1 = time.time() # Gets time at beginning of speed calc
+	count += 1
+	if count == 10:
+		global veloc
+		global t2
+		t2 = time.time()
+		veloc = 10.0/(t2-t1)
+		count = 0
 
-progress()
+	sys.stdout.write('\r'+str(progress)+'/'+str(input_num)+' :: '+'[{0}] {1}%'.format('#'*(progress/(int(input_num)/10)),progress/float(input_num)*100)+' :: '+str(int(veloc))+' Hz') # using print() prints new lines
+	sys.stdout.flush()
+
+def run(numtrip):
+	for i in range(int(numtrip)):
+		time.sleep(0.01)
+		update_progress(i)
+
+
+input_num = raw_input("Input a number of triples:")
+run(input_num)
+
